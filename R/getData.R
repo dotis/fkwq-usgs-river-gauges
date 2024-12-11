9accc4a065c55f0ba579f45e8a824b1ffb05e56f
Tylar <murray.tylar@gmail.com>
2024-12-11 13:31:53 -0500

    implement simple file cache

--- R/getData.R
+++ R/getData.R
@@ -5,10 +5,25 @@ getData <- function(station_id, param_name){
   # Get param_code from column of metadata row
   param_code <- metadata_row[[param_name]]
   
+  # check for cached file
+  fname <- glue::glue("data/cache/{station_id}___{param_name}.RDS")
+  if (file.exists(fname)){
+    print('using cached data')
+    df <- readRDS(fname)
+  } else {
+    print('requesting data from web')
+    df <- doDataRequest(param_code, station_id)
+    saveRDS(df, fname)
+  }
+  return(df)
+}
+
+doDataRequest <- function(param_code, station_id){
   request <- glue::glue(
     "https://waterdata.usgs.gov/nwis/dv?cb_{param_code}=on&format=rdb&site_no={station_id}&referred_module=sw&period=&begin_date=01-01-1950&end_date=12-31-2024"
   )
   
+  print("fetching from ")
   print(request)
   
   # Perform the request and read the data
@@ -25,7 +40,7 @@ getData <- function(station_id, param_name){
     data <- read.table(text = response_content, header = TRUE, sep = "\t", comment.char = "#")
     
     # Display the first few rows of the data
-    print(head(data))
+    # print(head(data))
   } else {
     # Print an error message if the request failed
     stop("Failed to fetch data. Status code: ", status_code(response))
